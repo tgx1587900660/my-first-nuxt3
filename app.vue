9caf4fbb8553e8132115ce4be5ec7232352adaae
唐干宵 <1587900660@qq.com>
2023-11-23 23:07:31 +0800

    feat: 认识nuxt3

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- nuxt 有基于文件的路由系统 -->
+
+  <!-- NuxtLink 相当于 RouterLink -->
+  <!-- 首页 index/index.vue -->
+  <NuxtLink to="/">首页</NuxtLink>
+  <!-- 视频页 video/index.vue -->
+  <NuxtLink to="/video">视频页</NuxtLink>
+
+  <!-- NuxtPage 相当于 RouterView -->
+  <NuxtPage></NuxtPage>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // 是否开启 ssr, 默认开启
+  // ssr: false,
   devtools: { enabled: true },
   vite: {
     css: {
@@ -17,7 +19,7 @@ export default defineNuxtConfig({
     head: {
       link: [{ rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' }]
     }
-  },
+  }
   // postcss: {
   //   plugins: {
   //     'postcss-nested': {},
