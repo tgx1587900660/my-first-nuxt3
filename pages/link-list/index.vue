ab4c9c2130192faa1915138a8f22465053ac7342
tangganxiao <tangganxiao@makeblock.com>
2024-02-29 11:32:33 +0800

    test: 测试group-hover分开在父子组件中使用的功能

---
+++ pages/link-list/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>测试 tailwindCSS</h1>
+  <hr />
+  <div class="text-3xl font-bold underline">Hello world!</div>
+  <hr />
+  <div class="w-[200px] h-[100px] bg-slate-300 group">
+    <TextVue></TextVue>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TextVue from './text.vue'
+</script>
+
+<style lang="less" scoped></style>
---
+++ pages/link-list/text.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="group-hover:bg-sky-500 w-[100px] h-[50px]">text 123</div>
+  <div>text2</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
