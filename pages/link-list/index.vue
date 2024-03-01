78c4fde6cbbd9e40c8e82fdc4e4358cc0f8c328b
tangganxiao <tangganxiao@makeblock.com>
2024-03-01 12:34:56 +0800

    Add new link and implement dark mode

--- pages/link-list/index.vue
+++ pages/link-list/index.vue
@@ -15,6 +15,29 @@
   >
     Save changes
   </button>
+
+  <h4>========= 基于父状态的样式 =============</h4>
+  <a
+    href="#"
+    class="group mx-auto block max-w-xs space-y-3 rounded-lg bg-white p-6 shadow-lg ring-1 ring-slate-900/5 hover:bg-sky-500 hover:ring-sky-500"
+  >
+    <div class="flex items-center space-x-3">
+      <svg class="h-6 w-6 stroke-sky-500 group-hover:stroke-white" fill="none" viewBox="0 0 24 24"><!-- ... --></svg>
+      <h3 class="text-sm font-semibold text-slate-900 group-hover:text-white">New project</h3>
+    </div>
+    <p class="text-sm text-slate-500 group-hover:text-white">
+      Create a new project from a variety of starting templates.
+    </p>
+  </a>
+
+  <h4>========= 深色模式 =============</h4>
+  <div class="rounded-lg bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800">
+    <h3 class="mt-5 text-base font-medium tracking-tight text-slate-900 dark:text-white">Writes Upside-Down</h3>
+    <p class="mt-2 text-sm text-slate-500 dark:text-slate-400">
+      The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+    </p>
+    <h1 class="text-[deeppink] dark:text-[pink]">text</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,4 +13,5 @@ export default {
     extend: {},
   },
   plugins: ['prettier-plugin-tailwindcss'],
+  darkMode: 'selector',
 }
